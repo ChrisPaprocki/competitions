2cd2c431056b657336905f896ef83f2fc7d4a91a
ChrisPaprocki <paprocki.krzysztof@gmail.com>
2018-02-09 12:50:35 +0100

    Tests fixed and extended.

--- src/ERC20Interface.sol
+++ src/ERC20Interface.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.11;
 /// @title ERC20 Token Interface
 /// @author Melonport AG <team@melonport.com>
 /// @notice See https://github.com/ethereum/EIPs/issues/20
-contract ERC20Interface {
+interface ERC20Interface {
 
     // EVENTS
 
@@ -12,13 +12,13 @@ contract ERC20Interface {
 
     // CONSTANT METHODS
 
-    function totalSupply() constant returns (uint256 totalSupply) {}
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+    function totalSupply() constant returns (uint256 totalSupply);
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
 
     // NON-CONSTANT METHODS
 
-    function transfer(address _to, uint256 _value) returns (bool success) {}
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
-    function approve(address _spender, uint256 _value) returns (bool success) {}
+    function transfer(address _to, uint256 _value) returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _value) returns (bool success);
 }
